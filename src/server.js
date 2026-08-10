09265562951a0f59d0834e5ae252e537f39c205a
Saba Khurram <sabakhurram643@gmail.com>
2026-08-11 02:08:50 +0500

    chore: organize backend structure

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 5000;
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.json({
